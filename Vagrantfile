80b7c0b374a40e15e81714230493a6ec92640c88
Gobin Sougrakpam <gobinsougrakpam@gmail.com>
2016-06-27 20:07:51 +1000

    Adding steps to download and run elasticsearch container.

--- Vagrantfile
+++ Vagrantfile
@@ -4,6 +4,7 @@ Vagrant.configure("2") do |config|
   config.vm.network :private_network, ip: "192.168.0.42"
 
   config.vm.provider :virtualbox do |vb|
+    vb.name = "test1"
     vb.customize [
       "modifyvm", :id,
       "--cpuexecutioncap", "50",
--- puppet/modules/elasticon/manifests/init.pp
+++ puppet/modules/elasticon/manifests/init.pp
@@ -1,3 +1,13 @@
 class elasticon{
-     include docker
+  require docker
+
+  docker::image { 'elasticsearch':
+    image_tag => 'latest'
+  } ->
+
+  docker::run { 'run-elasticsearch':
+    image   => 'elasticsearch',
+    command => 'docker run -d -p 9200:9200 -p 9300:9300 elasticsearch',
+  }
+
 }
